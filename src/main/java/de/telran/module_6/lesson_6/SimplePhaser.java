dc8fc3b71b3a7182c2d5c27b3d76bfe4126ac654
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2024-04-26 00:44:05 +0300

    module6 lesson6 concurrent 4

--- src/main/java/de/telran/module_6/lesson_6/SimplePhaser.java
+++ src/main/java/de/telran/module_6/lesson_6/SimplePhaser.java
@@ -3,10 +3,11 @@ package de.telran.module_6.lesson_6;
 import java.util.concurrent.Phaser;
 
 public class SimplePhaser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Phaser phaser = new Phaser(1);
         int curPhase;
 
+//        phaser.register(); // главный поток не регистрируем, потому что мы его зарегистрировали при создании Phaser
         System.out.println("Стартуем потоки");
 
         new Thread(new MyThread(phaser, "A")).start();
@@ -20,7 +21,7 @@ public class SimplePhaser {
         phaser.arriveAndAwaitAdvance();
         System.out.println("Phase " + curPhase + " окончена");
 
-
+        new Thread(new MyThread2(phaser, "D")).start();
         curPhase = phaser.getPhase();
 
         // ожидаем завершения всеми потоками второй фазы
@@ -30,6 +31,7 @@ public class SimplePhaser {
         // снимаем основной поток исполнения с регистрации
         phaser.arriveAndDeregister();
 
+        Thread.sleep(1000);
         if (phaser.isTerminated())
             System.out.println("Phaser окончил работу");
 
@@ -51,7 +53,7 @@ class MyThread implements Runnable {
     @Override
     public void run() {
         System.out.println("Поток " + name + " запустился в первой фазе");
-        phaser.arriveAndAwaitAdvance();
+        phaser.arriveAndAwaitAdvance(); // ожидаем завершения первой фазы
 
         // небольшая пауза, чтобы не нарушать порядок вывода. Только для демонстрации, в реальности этого может не быть
         try {
@@ -61,6 +63,10 @@ class MyThread implements Runnable {
         }
 
         System.out.println("Поток " + name + " запустился во второй фазе");
+        if(name.equals("C")) {
+            phaser.arriveAndDeregister();
+            return;
+        }
         phaser.arriveAndAwaitAdvance();
 
         try {
@@ -70,6 +76,35 @@ class MyThread implements Runnable {
         }
 
         System.out.println("Поток " + name + " запустился в третьей фазе");
-        phaser.arriveAndAwaitAdvance();
+//        phaser.arriveAndAwaitAdvance();
+        phaser.arriveAndDeregister();
+
     }
+}
+
+class MyThread2 implements Runnable {
+    Phaser phaser;
+    String name;
+
+    public MyThread2(Phaser phaser, String name) {
+
+        this.phaser = phaser;
+        this.name = name;
+
+        phaser.register();
+    }
+
+    @Override
+    public void run() {
+
+        // небольшая пауза, чтобы не нарушать порядок вывода. Только для демонстрации, в реальности этого может не быть
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Поток " + name + " запустился во второй фазе");
+        phaser.arriveAndDeregister();
+      }
 }
